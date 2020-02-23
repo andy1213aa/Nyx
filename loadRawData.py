d5e49592c2b8cba33d8bf01b5a67f8a9834b0c20
Sun Cheng <andy1213aa@gmail.com>
2020-02-23 16:08:30 +0800

    3D plot with 30 raw data

--- loadRawData.py
+++ loadRawData.py
@@ -1,11 +1,12 @@
 import  numpy as np
 import os 
-
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 def loadData(directory, width, length, height):
     g = os.walk(directory)
     flag = 0
     for path, dirList, fileList in g:
-        trainingData = np.zeros((len(fileList), width, length, height))
+        trainingData = np.zeros((30, width, length, height))
         print(f'You have {len(fileList)} data.')     
         for data in fileList:
             print(f'Starting Loging data {flag}', end='\r')
@@ -15,14 +16,29 @@ def loadData(directory, width, length, height):
             
             trainingData[flag] = load_data_16x16
             flag += 1
+            if flag == 30:
+                break
     print('')
     print('Done!')
     
     return trainingData
 result = loadData(r'C:\Users\Andy\Desktop\Nyx\NyxDataSet', 16, 16, 1)
 flag = 0
+
 for i in result:
     print("Data: ", flag)
     print("Std: ", i.std())
     print("Mean: ", i.mean())
     flag += 1
+    print(i.shape)
+    fig = plt.figure()
+    axis = plt.axes(projection='3d')
+    x = range(0, 16)
+    y= range(0, 16)
+    xData, yData = np.meshgrid(x, y)
+    zData = i[x, y]
+    surface = axis.plot_surface(xData, yData, zData, rstride=1, cstride=1, cmap='coolwarm_r')
+    fig.colorbar(surface, shrink=1.0, aspect=20)
+    plt.show()
+
+
